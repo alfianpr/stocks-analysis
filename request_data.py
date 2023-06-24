bd9cb08e555cfb219834efbfdbff0bae23580f17
alfanpr <alfanpratama@hotmail.com>
2023-06-24 20:08:51 +0700

    update

--- request_data.py
+++ request_data.py
@@ -1,14 +1,7 @@
 import json
 import pandas as pd
-import random
-import re
 import requests
-import time
-import numpy as np
-from datetime import date
 import os
-import time
-import sys
 from bs4 import BeautifulSoup
 
 headers = {
@@ -18,8 +11,8 @@ headers = {
 params = {
     "symbol" : "BBCA",
     "data_type" : 1,
-    "report_type" : 1,
-    "statement_type": 1
+    "report_type" : 3,
+    "statement_type": 2
 }
 
 scrape = requests.get("https://exodus.stockbit.com/findata-view/company/financial", params=params, headers=headers)
@@ -30,33 +23,74 @@ df_0 = scrape.json()
 df_1 = pd.json_normalize(df_0)
 df_2 = json.loads(pd.Series.to_json(df_1))
 df_3 = pd.DataFrame(df_2)
-df_3["data.html_report"].to_csv(f"{params['symbol']}.csv", index=False, header=False)
+df_3["data.html_report"].to_csv(f"data/{params['symbol']}.csv", index=False, header=False)
 
-os.rename(f"{params['symbol']}.csv", f"{params['symbol']}.html")
+os.rename(f"data/{params['symbol']}.csv", f"data/{params['symbol']}.html")
 
-data = []
+soup = BeautifulSoup(open(f"data/{params['symbol']}.html"),'html.parser')
+
+# scrape the main data
+header_elements = soup.find_all("table")[0].find("tr")
+data_elements = soup.find_all("table")[0].find_all("tr")[1:]
+
+# for getting the header
 list_header = []
-soup = BeautifulSoup(open(f"{params['symbol']}.html"),'html.parser')
-header = soup.find_all("table")[0].find("tr")
-
-for items in header:
+for i in header_elements:
     try:
-        list_header.append(items.get_text())
+        list_header.append(i.get_text())
     except:
         continue
 
 # for getting the data
-HTML_data = soup.find_all("table")[0].find_all("tr")[1:]
- 
-for element in HTML_data:
+data = []
+for i in data_elements:
     sub_data = []
-    for sub_element in element:
+    for j in i:
         try:
-            sub_data.append(sub_element.get_text())
+            sub_data.append(j.get_text())
         except:
             continue
     data.append(sub_data)
 
+# clean the data
+clean_list_header = [i for i in list_header if i != " "]
 
-dataFrame = pd.DataFrame(data = data, columns = list_header)
-dataFrame.to_csv(f"clean_{params['symbol']}.csv")
+clean_data = []
+for i in range(0, len(data)):
+    clean_data.append([j for j in data[i] if j != " "])
+
+dataFrame = pd.DataFrame(data = clean_data, columns = clean_list_header)
+dataFrame.to_csv(f"data/clean_{params['symbol']}_data.csv")
+
+# scrape key ratio
+header_elements_2 = soup.find_all("table")[1].find("tr")
+data_elements_2 = soup.find_all("table")[1].find_all("tr")[1:]
+
+list_header_2 = []
+for i in header_elements_2:
+    try:
+        list_header_2.append(i.get_text())
+    except:
+        continue
+
+# for getting the data
+data_2 = []
+for i in data_elements_2:
+    sub_data_2 = []
+    for j in i:
+        try:
+            sub_data_2.append(j.get_text())
+        except:
+            continue
+    data_2.append(sub_data_2)
+
+# clean the data
+clean_list_header_2 = [i for i in list_header_2 if i != " "]
+
+clean_data_2 = []
+for i in range(0, len(data_2)):
+    clean_data_2.append([j for j in data_2[i] if j != " "])
+
+
+dataFrame_2 = pd.DataFrame(data = clean_data_2, columns = clean_list_header_2)
+dataFrame_2.to_csv(f"data/clean_{params['symbol']}_keyratio.csv")
