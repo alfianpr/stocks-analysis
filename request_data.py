2dc8fd07199b7a103ca33d83460c80cc1af2907d
alfanpr <alfanpratama@hotmail.com>
2023-06-24 15:18:12 +0700

    update

--- request_data.py
+++ request_data.py
@@ -6,14 +6,17 @@ import requests
 import time
 import numpy as np
 from datetime import date
-import html_to_json
+import os
+import time
+import sys
+from bs4 import BeautifulSoup
 
 headers = {
-    "Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZSI6IkFsZmlhbnByOTkiLCJlbWEiOiJhbGZpYW5wcmF0YW1hQGhvdG1haWwuY29tIiwiZnVsIjoiQWxmaWFuIFByYXRhbWEiLCJzZXMiOiJvNndYTGJRYTVhYUc3ZnZmIiwiZHZjIjoiIiwidWlkIjo2NjAyODF9LCJleHAiOjE2ODc0MTU5NjYsImlhdCI6MTY4NzMyOTU2NiwiaXNzIjoiU1RPQ0tCSVQiLCJqdGkiOiJlNTliMzNiOC02MTQxLTQ0ZjAtYmZkYy1kYzg1MDRjMDJhNjEiLCJuYmYiOjE2ODczMjk1NjZ9.aM6tOCRI8lwd-r5PPRVmyE3bFCCHSSjarYcfxPTLQJk"
+    "Authorization" : "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjU3MDc0NjI3LTg4MWItNDQzZC04OTcyLTdmMmMzOTNlMzYyOSIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZSI6IkFsZmlhbnByOTkiLCJlbWEiOiJhbGZpYW5wcmF0YW1hQGhvdG1haWwuY29tIiwiZnVsIjoiQWxmaWFuIFByYXRhbWEiLCJzZXMiOiJWM2RJY3NGNXVBOFVubFE2IiwiZHZjIjoiIiwidWlkIjo2NjAyODF9LCJleHAiOjE2ODc2NzYzNzYsImlhdCI6MTY4NzU4OTk3NiwiaXNzIjoiU1RPQ0tCSVQiLCJqdGkiOiIyZTljYTJkZC0wNWJmLTRjOTYtYmE5MS1iMThlZDdmYjUyYTgiLCJuYmYiOjE2ODc1ODk5NzZ9.tOyHLRdUqQhyo8d-yTd2pKfCsT58ELzHPFTo_qOZJRnPSiXiQM-F7-xlHevf8fy7WrptSmyd6m3MLU_sCmLWtE34V6D4Jk39u7tUbUIVJfiuYbsDA9ANUl4Ldg1_m0RszxtqDA0i4xL8VAHhjcACjXsnnApVVA8eze1-ugiwQDnAmo98gms_JCV69n2iIKsTCwUaf-Z2wIrTGEztS2n5IvPO-3PNU-78gsHI3tRNld4PI_HxvPoIrACM_xQZM8eef2JJVivic54kWLqHaGnZuPR2hxM41KWUIDx4TSYiDPbBE65IGo7y8_zVlhai5ahqvo5uBmuHK9z7kBmZxBlH0Q"
 }
 
 params = {
-    "symbol" : "CAMP",
+    "symbol" : "BBCA",
     "data_type" : 1,
     "report_type" : 1,
     "statement_type": 1
@@ -25,29 +28,35 @@ print(scrape.status_code)
 
 df_0 = scrape.json()
 df_1 = pd.json_normalize(df_0)
-df_2 = json.loads(pd.Series.to_json(df_1["data.html_report"]))
-df_3 = df_2["0"]
+df_2 = json.loads(pd.Series.to_json(df_1))
+df_3 = pd.DataFrame(df_2)
+df_3["data.html_report"].to_csv(f"{params['symbol']}.csv", index=False, header=False)
 
-df_4 = html_to_json.convert(df_3)
-df_5 = df_4["div"]
-df_6 = pd.DataFrame(df_5)
-df_7 = df_6["div"][0]
+os.rename(f"{params['symbol']}.csv", f"{params['symbol']}.html")
 
-df_8 = pd.DataFrame(df_7)
+data = []
+list_header = []
+soup = BeautifulSoup(open(f"{params['symbol']}.html"),'html.parser')
+header = soup.find_all("table")[0].find("tr")
 
-df_9 = df_8["div"]
-df_10 = pd.DataFrame(df_9)
-df_11 = df_10["div"][1]
-df_12 = pd.DataFrame(df_11)
-df_13 = df_12["div"][0]
-df_14 = pd.DataFrame(df_13)
-df_15 = df_14["table"][0]
-df_16 = pd.DataFrame(df_15)
-df_17 = df_16["tbody"][0]
-df_18 = pd.DataFrame(df_17)
-df_19 = df_18["tr"][0]
-df_20 = pd.DataFrame(df_19)
-# print (df_12)
-# df_10 = pd.DataFrame(df_9)
+for items in header:
+    try:
+        list_header.append(items.get_text())
+    except:
+        continue
 
-df_20.to_csv("test.csv")
+# for getting the data
+HTML_data = soup.find_all("table")[0].find_all("tr")[1:]
+ 
+for element in HTML_data:
+    sub_data = []
+    for sub_element in element:
+        try:
+            sub_data.append(sub_element.get_text())
+        except:
+            continue
+    data.append(sub_data)
+
+
+dataFrame = pd.DataFrame(data = data, columns = list_header)
+dataFrame.to_csv(f"clean_{params['symbol']}.csv")
