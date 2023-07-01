92f16d591e6cda8aedefd974df20baf617127e90
alfanpr <alfanpratama@hotmail.com>
2023-07-01 22:22:16 +0700

    update

--- dcf.py
+++ dcf.py
@@ -1,5 +1,70 @@
+import argparse
+from utils import scrape_key, metrics, calculate_dcf
+
 # Asumption configuration
 growth_rate_year_5_10 = 0.12
 growth_rate_year_5_10 = 0.09
 discount_rate = 0.09
 terminal_growth_rate = 0.02
+
+# Scrape_configuration
+'''
+This variable has purpose to scrape financial report type.
+List option : income_statement, balance_sheet, cash_flow
+'''
+TYPE_FREE_CASH_FLOW = "cash_flow"
+TYPE_SHARE_OUTSTANDING = "income_statement"
+TYPE_NET_DEBT_LEVEL = "balance_sheet"
+
+# Convert configuration
+match TYPE_FREE_CASH_FLOW:
+    case "cash_flow":
+        CONV_FREE_CASH_FLOW = 3
+
+match TYPE_SHARE_OUTSTANDING:
+    case "income_statement":
+        CONV_SHARE_OUTSTANDING = 1
+
+match TYPE_NET_DEBT_LEVEL:
+    case "balance_sheet":
+        CONV_NET_DEBT_LEVEL = 2
+
+parser = argparse.ArgumentParser(description="stocks analysis")
+parser.add_argument("-n", help="input the company name")
+args = parser.parse_args()
+
+df_fcf_key = scrape_key(CONV_FREE_CASH_FLOW, args.n)
+df_so_key = scrape_key(CONV_SHARE_OUTSTANDING, args.n)
+df_nd_key = scrape_key(CONV_NET_DEBT_LEVEL, args.n)
+
+average_free_cash_flow = metrics(metrics="Free cash flow (Annual)", dataframe=df_fcf_key, average=3)
+share_outstanding = metrics(metrics="Share Outstanding", dataframe=df_so_key, average=1)
+net_debt = metrics(metrics="Net Debt (Annual)", dataframe=df_nd_key, average=1)
+
+intrinsic = calculate_dcf(G15 = growth_rate_year_5_10, 
+                          G610 = growth_rate_year_5_10, 
+                          TGR = terminal_growth_rate, 
+                          DR = discount_rate, 
+                          average_free_cash_flow = average_free_cash_flow, 
+                          share_outstanding = share_outstanding, 
+                          net_debt = net_debt)
+
+if __name__ == "__main__":
+    print("Data received")
+    try:
+        print(f"The intrinsic value of {args.n} using DCF method is:", "\nRp", intrinsic)
+    except:
+        print("Can't get the financial data")
+    print("################# Key financial report ####################")
+    try:
+        print("Average free cash flow in the last 3 years:", "Rp", average_free_cash_flow)
+    except:
+        print("Can't get the financial data")
+    try:
+        print("Share Outstanding:", "Rp", share_outstanding)
+    except:
+        print("Can't get the financial data")
+    try:
+        print("Total Net Debt:", "Rp", net_debt)
+    except:
+        print("Can't get the financial data")
--- utils.py
+++ utils.py
@@ -3,39 +3,11 @@ import pandas as pd
 import requests
 import os
 from bs4 import BeautifulSoup
-import argparse
 import re
 from decimal import Decimal
 
-# Scrape_configuration
-'''
-This variable has purpose to scrape financial report type.
-List option : income_statement, balance_sheet, cash_flow
-'''
-TYPE_FREE_CASH_FLOW = "cash_flow"
-TYPE_SHARE_OUTSTANDING = "income_statement"
-TYPE_NET_DEBT_LEVEL = "balance_sheet"
-
-# Convert configuration
-match TYPE_FREE_CASH_FLOW:
-    case "cash_flow":
-        CONV_FREE_CASH_FLOW = 3
-
-match TYPE_SHARE_OUTSTANDING:
-    case "income_statement":
-        CONV_SHARE_OUTSTANDING = 1
-
-match TYPE_NET_DEBT_LEVEL:
-    case "balance_sheet":
-        CONV_NET_DEBT_LEVEL = 2
-
-# Get user input configuration
-parser = argparse.ArgumentParser(description="stocks analysis")
-parser.add_argument("-n", help="input the company name")
-args = parser.parse_args()
-
 # Scrape requirements
-def scrape_finance(TYPE):
+def scrape_finance(TYPE, SYMBOL):
     global params, req
 
     ENDPOINT = "https://exodus.stockbit.com/findata-view/company/financial"
@@ -48,7 +20,7 @@ def scrape_finance(TYPE):
     }
 
     params = {
-        "symbol" : args.n,
+        "symbol" : SYMBOL,
         "data_type" : 1,
         "report_type" : TYPE,
         "statement_type": 2
@@ -65,9 +37,9 @@ def scrape_finance(TYPE):
     return BeautifulSoup(open(f"data/{params['symbol']}_{TYPE}.html"),'html.parser')
 
 # scrape the main data
-def scrape_data(TYPE):
-    header_elements = scrape_finance(TYPE).find_all("table")[0].find("tr")
-    data_elements = scrape_finance(TYPE).find_all("table")[0].find_all("tr")[1:]
+def scrape_data(TYPE, SYMBOL):
+    header_elements = scrape_finance(TYPE, SYMBOL).find_all("table")[0].find("tr")
+    data_elements = scrape_finance(TYPE, SYMBOL).find_all("table")[0].find_all("tr")[1:]
 
     # for getting the header
     list_header = []
@@ -100,9 +72,9 @@ def scrape_data(TYPE):
     return dataFrame
 
 # scrape key ratio
-def scrape_key(TYPE):
-    header_elements_2 = scrape_finance(TYPE).find_all("table")[1].find("tr")
-    data_elements_2 = scrape_finance(TYPE).find_all("table")[1].find_all("tr")[1:]
+def scrape_key(TYPE, SYMBOL):
+    header_elements_2 = scrape_finance(TYPE, SYMBOL).find_all("table")[1].find("tr")
+    data_elements_2 = scrape_finance(TYPE, SYMBOL).find_all("table")[1].find_all("tr")[1:]
 
     list_header_2 = []
     for i in header_elements_2:
@@ -133,11 +105,6 @@ def scrape_key(TYPE):
     dataFrame_2.to_csv(f"data/clean_{params['symbol']}_{TYPE}_keyratio.csv")
     return dataFrame_2
 
-# df_fcf_data = scrape_data(CONV_FREE_CASH_FLOW)
-df_fcf_key = scrape_key(CONV_FREE_CASH_FLOW)
-df_so_key = scrape_key(CONV_SHARE_OUTSTANDING)
-df_nd_key = scrape_key(CONV_NET_DEBT_LEVEL)
-
 def Average(lst):
     return sum(lst) / len(lst)
 
@@ -163,18 +130,6 @@ def metrics(metrics, dataframe, average):
     average_fcf = Average(lst = regex_4)
     return average_fcf
 
-# Scraped metrics
-average_free_cash_flow = metrics(metrics="Free cash flow (Annual)", dataframe=df_fcf_key, average=3)
-share_outstanding = metrics(metrics="Share Outstanding", dataframe=df_so_key, average=1)
-net_debt = metrics(metrics="Net Debt (Annual)", dataframe=df_nd_key, average=1)
-
-# Asumption metrics
-G15 = 0.12
-G610 = 0.09
-TGR = 0.02
-DR = 0.09
-
-# DCF calculation
 def calculate_dcf(G15, G610, TGR, DR, average_free_cash_flow, share_outstanding, net_debt):
     E_1_5 = average_free_cash_flow*(G15+1)**5
     E_6_10 = E_1_5*(G610+1)**5
@@ -200,26 +155,3 @@ def calculate_dcf(G15, G610, TGR, DR, average_free_cash_flow, share_outstanding,
     Fn_Terminal = Eterminal/(1+DR)**10
     tot_Fn = sum(lst_Fn+[float(Fn_Terminal)])
     return round((tot_Fn - net_debt)/share_outstanding, 2)
-
-if __name__ == "__main__":
-    if req.status_code == 200:
-        print("Data received")
-        try:
-            print(f"The intrinsic value of {params['symbol']} using DCF method is:", "\nRp", intrinsic)
-        except:
-            print("Can't get the financial data")
-        print("################# Key financial report ####################")
-        try:
-            print("Average free cash flow in the last 3 years:", "Rp", average_free_cash_flow)
-        except:
-            print("Can't get the financial data")
-        try:
-            print("Share Outstanding:", "Rp", share_outstanding)
-        except:
-            print("Can't get the financial data")
-        try:
-            print("Total Net Debt:", "Rp", net_debt)
-        except:
-            print("Can't get the financial data")
-    else:
-        print("can't connect, please fix the credential")
